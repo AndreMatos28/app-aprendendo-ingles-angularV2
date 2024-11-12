6ee1c45c5c5f8b684df301e02944ed20dfd4a18c
André Nunes <andrealleati@gmail.com>
2024-11-12 20:29:32 -0300

    update all project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PanelComponent } from "./panel/panel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PanelComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tradutor_angularV2';
+  public jogoEmAndamento: boolean = true;
+  public encerramento: string = '';
+
+  public encerrarJogo(tipo: string): void {
+    this.jogoEmAndamento = false;
+    this.encerramento = tipo;
+  }
+
+  public reiniciar(): void {
+    this.jogoEmAndamento = true;
+    this.encerramento = '';
+  }
 }
